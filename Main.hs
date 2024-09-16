860505afe9730b38bc405f697047a65b6d7f5c81
Yassine Founounou <Yassine.Founounou@acronis.com>
2024-09-16 20:33:03 +0200

    Homework 1 Founounou

--- Main.hs
+++ Main.hs
@@ -4,22 +4,47 @@ import Control.Monad (unless)
 import Text.Printf (printf)
 
 short :: [a] -> Bool
-short = undefined 
+short [] = True
+short [_] = True
+short [_, _] = True
+short _ = False
 
 lovely :: [Int] -> Bool
-lovely = undefined 
+lovely lst = short lst || lst !! 2 == 14
 
 rightTriangles :: [(Int, Int, Int)]
-rightTriangles = undefined 
+rightTriangles = [(a, b, c) | c <- [1..], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2] 
 
 fizzBuzz :: [String]
-fizzBuzz = undefined 
+fizzBuzz = [fizzBuzzHelper n | n <- [1..]] 
+
+fizzBuzzHelper :: Int -> String
+fizzBuzzHelper n
+    | n `mod` 15 == 0 = "FizzBuzz"
+    | n `mod` 5 == 0 = "Buzz"
+    | n `mod` 3 == 0 = "Fizz"
+    | otherwise = show n
 
 ageOn :: String -> Float -> Float
-ageOn planet ageInSeconds = undefined 
+ageOn planet ageInSeconds = case planet of
+    "Mercury" -> (ageInSeconds / 31557600.0) * (1/0.2408467)
+    "Venus" -> (ageInSeconds / 31557600.0) * (1/0.61519726)
+    "Earth" -> ageInSeconds / 31557600.0
+    "Mars" -> (ageInSeconds / 31557600.0) * (1/1.8808158)
+    "Jupiter" -> (ageInSeconds / 31557600.0) * (1/11.862615 )
+    "Saturn" -> (ageInSeconds / 31557600.0) * (1/29.447498)
+    "Uranus" -> (ageInSeconds / 31557600.0) * (1/84.016846)
+    "Neptune" -> (ageInSeconds / 31557600.0) * (1/164.79132)
+    "Pluto" -> error "Pluto is not a planet!"
+    _ -> error "Unrecognized planet name !"
 
 isLeapYear :: Int -> Bool
-isLeapYear year = undefined 
+isLeapYear year 
+    | year `mod` 400 == 0 = True
+    | year `mod` 100 == 0 = False
+    | year `mod` 4 == 0 = True
+    | year < 0 = error "Year cannot be negative"
+    | otherwise = False
 
 main = do
   runTests
