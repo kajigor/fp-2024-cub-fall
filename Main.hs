6e412fb8d964b54b6f32bb0f33f3e27ed343edf0
Enrike Valentinov <evalentinov@constructor.university>
2024-09-26 23:39:07 +0200

    Homework 2 Done

--- Main.hs
+++ Main.hs
@@ -11,28 +11,47 @@ import qualified Data.List as L
 import Text.Printf (printf)
 
 multByIndex :: [Int] -> [Int]
-multByIndex = undefined
+multByIndex xs = zipWith (*) xs [0..]
 
 powerByIndex :: [Int] -> [Int]
-powerByIndex = undefined
+powerByIndex xs = zipWith (^) xs [0..]
 
+prodList [] = 1
+prodList (h:t) = h * prodList t
+
+short :: [a] -> Bool
+short [] = True
+short [_] = True
+short _ = False
 productOfDifference :: [Int] -> Int
-productOfDifference = undefined
+productOfDifference xs
+    | short xs = error "The list is too short"
+    | otherwise = prodList (zipWith (-) xs (tail xs))
 
-isSorted :: [Int] -> Bool 
-isSorted xs = undefined
+isSorted :: [Int] -> Bool
+isSorted xs = and (zipWith (<=) xs (tail xs))
 
-countElement :: Int -> [Int] -> Int 
-countElement = undefined
+countElement :: Int -> [Int] -> Int
+countElement x [] = 0
+countElement x (y:ys)
+    | x == y = 1 + compareNextNumber
+    | otherwise = compareNextNumber
+    where compareNextNumber = countElement x ys
 
-dotProduct :: [Int] -> [Int] -> Int 
-dotProduct = undefined
+sumList [] = 0
+sumList (h:t) = h + sumList t
+dotProduct :: [Int] -> [Int] -> Int
+dotProduct xs ys = sumList (zipWith (*) xs ys)
 
 applyAll :: [a -> b] -> a -> [b]
-applyAll = undefined
+applyAll [] x = []
+applyAll [f] x = [f x]
+applyAll (f:fs) x = f x : applyAll fs x
 
-interleave :: [a] -> [a] -> [a] 
-interleave = undefined
+interleave :: [a] -> [a] -> [a]
+interleave (x:xs) (y:ys) = x : y : interleave xs ys
+interleave xs [] = []
+interleave [] ys = []
 
 main = do
   runTests
@@ -42,36 +61,36 @@ runTests = do
     runMultByIndex
     runPowerByIndex
     runProductOfDifference
-    runIsSorted 
-    runCountElement 
-    runDotProduct 
-    runApplyAll 
+    runIsSorted
+    runCountElement
+    runDotProduct
+    runApplyAll
     runInterleave
   where
-    runMultByIndex = do 
-        mapM_ (uncurry $ test1 "multByIndex" multByIndex) testCases 
+    runMultByIndex = do
+        mapM_ (uncurry $ test1 "multByIndex" multByIndex) testCases
         test1 "multByIndex, infinite input" (take 10 . multByIndex) [1 ..] [0, 2, 6, 12, 20, 30, 42, 56, 72, 90]
-      where 
-        testCases = 
+      where
+        testCases =
           [ ([], [])
           , ([1..10], [0,2,6,12,20,30,42,56,72,90])
           , ([-1, -2, -3], [0, -2, -6])
           ]
 
-    runPowerByIndex = do 
-        mapM_ (uncurry $ test1 "powerByIndex" powerByIndex) testCases 
+    runPowerByIndex = do
+        mapM_ (uncurry $ test1 "powerByIndex" powerByIndex) testCases
         test1 "powerByIndex, infinite input" (take 10 . powerByIndex) [1 ..] [1,2^1,3^2,4^3,5^4,6^5,7^6,8^7,9^8,10^9]
-      where 
-        testCases = 
+      where
+        testCases =
           [ ([], [])
           , ([1..10], [1,2^1,3^2,4^3,5^4,6^5,7^6,8^7,9^8,10^9])
           , ([-1, -2, -3], [1, (-2)^1, (-3)^2])
           ]
-    
-    runProductOfDifference = do 
+
+    runProductOfDifference = do
         mapM_ (uncurry $ test1 "productOfDifference" productOfDifference) testCases
-      where 
-        testCases = 
+      where
+        testCases =
           [ ([10, 10], 0)
           , ([42, 13], 42 - 13)
           , ([(-5), (-3) .. 5], -(2^5))
@@ -80,10 +99,10 @@ runTests = do
           , ([2033, 2925, 6087, 272, 3643], (2033 - 2925) * (2925 - 6087) * (6087 - 272) * (272 - 3643))
           ]
 
-    runIsSorted = do 
-        mapM_ (uncurry $ test1 "isSorted" isSorted) testCases 
-      where 
-        testCases = 
+    runIsSorted = do
+        mapM_ (uncurry $ test1 "isSorted" isSorted) testCases
+      where
+        testCases =
           [ ([], True)
           , ([1..10], True)
           , ([10,8 .. 1], False)
@@ -92,12 +111,12 @@ runTests = do
           , ([13, 542, 1, 3,4,5,5], False)
           ]
 
-    uncurry3 f (x,y,z) = f x y z 
+    uncurry3 f (x,y,z) = f x y z
 
-    runCountElement = do 
+    runCountElement = do
         mapM_ (uncurry3 $ test2 "countElement" countElement) testCases
-      where 
-        testCases = 
+      where
+        testCases =
           [ (12, [], 0)
           , (12, replicate 12 12, 12 )
           , (13, replicate 12 12, 0)
@@ -106,10 +125,10 @@ runTests = do
           , (42, [1, 42, 2, 42, 42, 42, 4, 42], 5)
           ]
 
-    runDotProduct = do 
-        mapM_ (uncurry3 $ test2 "dotProduct" dotProduct) testCases 
-      where 
-        testCases = 
+    runDotProduct = do
+        mapM_ (uncurry3 $ test2 "dotProduct" dotProduct) testCases
+      where
+        testCases =
           [ ([], [], 0)
           , ([2], [3], 6)
           , ([1,2,3], [4,5,6], 32)
@@ -120,35 +139,35 @@ runTests = do
           , ([2], [3,1], 6)
           ]
 
-    runApplyAll = do 
-        mapM_ (uncurry3 $ test2 "applyAll" applyAll) testCases 
-        mapM_ (uncurry3 $ test2 "applyAll" applyAll) testCasesList 
-      where 
-        test2 functionName f inp1 inp2 exp = 
-          let act = f inp1 inp2 in 
-          if act == exp 
-          then return () 
+    runApplyAll = do
+        mapM_ (uncurry3 $ test2 "applyAll" applyAll) testCases
+        mapM_ (uncurry3 $ test2 "applyAll" applyAll) testCasesList
+      where
+        test2 functionName f inp1 inp2 exp =
+          let act = f inp1 inp2 in
+          if act == exp
+          then return ()
           else putStrLn "applyAll test Failed"
 
-        testCases = 
+        testCases =
           [ ([] :: [Int -> Int], 42, [])
           , (replicate 13 id, 42, replicate 13 42)
           , ([(*2), (+3), (^2)], 42, [42*2, 42+3, 42^2])
           , (replicate 5 (const 42), 13, replicate 5 42)
           ]
-        testCasesList = 
+        testCasesList =
           [ ([length, const 0, length . take 3], "Hello", [5, 0, 3])
           ]
 
-    runInterleave = do 
-        mapM_ (uncurry3 $ test2 "interleave" interleave) testCases 
-      where 
-        testCases = 
+    runInterleave = do
+        mapM_ (uncurry3 $ test2 "interleave" interleave) testCases
+      where
+        testCases =
           [ ([], [], [])
           , ([1], [2], [1,2])
           , ([1..10], [10,9..1], [1,10,2,9,3,8,4,7,5,6,6,5,7,4,8,3,9,2,10,1])
           ]
-   
+
     describeFailure :: (Show a, Show b) => String -> a -> b -> b -> IO ()
     describeFailure functionName input exp actual =
       putStrLn $
@@ -158,17 +177,17 @@ runTests = do
           (show input)
           (show exp)
           (show actual)
-    
-    test1 :: (Show a, Show b, Eq b) => String -> (a -> b) -> a -> b -> IO () 
-    test1 functionName f inp exp = 
-      let act = f inp in 
-      if act == exp 
-      then return () 
+
+    test1 :: (Show a, Show b, Eq b) => String -> (a -> b) -> a -> b -> IO ()
+    test1 functionName f inp exp =
+      let act = f inp in
+      if act == exp
+      then return ()
       else describeFailure functionName inp exp act
 
     test2 :: (Show a, Show b, Show c, Eq c) => String -> (a -> b -> c) -> a -> b -> c -> IO ()
-    test2 functionName f inp1 inp2 exp = 
-      let act = f inp1 inp2 in 
-      if act == exp 
-      then return () 
+    test2 functionName f inp1 inp2 exp =
+      let act = f inp1 inp2 in
+      if act == exp
+      then return ()
       else describeFailure functionName (inp1, inp2) exp act
