c7e5a9ac923270f5dfea527c8d8c6dca20c63ca5
Ana Dobrescu <adobrescu@constructor.university>
2024-10-01 21:30:08 +0200

    HW03

--- Main.hs
+++ Main.hs
@@ -1,34 +1,101 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module Main where
 
 import Control.Monad (unless)
 import Text.Printf (printf)
 
 data Expr
+  = Num Double
+  | Sqrt Expr
+  | Minus Expr Expr
+  | Plus Expr Expr
+  | Multi Expr Expr
+  | Div Expr Expr
+  | Power Expr Expr
 
 instance Show Expr where
-  show = undefined
+  show (Num a) = printf "Number: %s" (show a)
+  show (Sqrt a) = printf "Square root: %s" (show a)
+  show (Minus a b) = printf "Subtraction: %s - %s" (show a) (show b)
+  show (Plus a b) = printf "Addition: %s + %s" (show a) (show b)
+  show (Multi a b) = printf "Multiplication: %s" (show a) (show b)
+  show (Div a b) = printf "Division: %s / %s" (show a) (show b)
+  show (Power a b) = printf "Power: %s ^ %s" (show a) (show b)
 
 instance Eq Expr where
-  (==) = undefined
+  (==) :: Expr -> Expr -> Bool
+  Num a == Num b = a == b
+  Sqrt a == Sqrt b = a == b
+  Minus a b == Minus c d = a == c && b == d
+  Plus a b == Plus c d = a == c && b == d
+  Multi a b == Multi c d = a == c && b == d
+  Div a b == Div c d = a == c && b == d
+  Power a b == Power c d = a == c && b == d
+  _ == _ = False
 
 data Error
+  = DivisionByZero
+  | SqrtOfNegNumber
 
 instance Show Error where
-  show = undefined
+  show DivisionByZero = "Error: tried to do devision by zero"
+  show SqrtOfNegNumber = "Error: tried to find the square root of a negative number"
 
 instance Eq Error where
-  (==) = undefined
+  DivisionByZero == DivisionByZero = True
+  SqrtOfNegNumber == SqrtOfNegNumber = True
+  _ == _ = False
 
 eval :: Expr -> Either Error Double
-eval = undefined
+eval (Num a) = Right a
+eval (Sqrt a) = do
+  val <- eval a
+  if val >= 0 then Right (sqrt val) else Left SqrtOfNegNumber
+eval (Minus a b) = do
+  val1 <- eval a
+  val2 <- eval b
+  Right (val1 - val2)
+eval (Plus a b) = do
+  val1 <- eval a
+  val2 <- eval b
+  Right (val1 + val2)
+eval (Multi a b) = do
+  val1 <- eval a
+  val2 <- eval b
+  Right (val1 * val2)
+eval (Div a b) = do
+  val1 <- eval a
+  val2 <- eval b
+  if val2 == 0
+    then Left DivisionByZero
+    else Right (val1 / val2)
+eval (Power a b) = do
+  val1 <- eval a
+  val2 <- eval b
+  Right (val1 ** val2)
 
 cases :: [(Expr, Either Error Double)]
-cases = undefined
+cases =
+  [ (Num 10, Right 10.0),
+    (Num (-1), Right (-1.0)),
+    (Sqrt (Num 25), Right 5.0),
+    (Sqrt (Num (-1)), Left SqrtOfNegNumber),
+    (Minus (Num 78) (Num 8), Right 70.0),
+    (Plus (Num 67) (Num 13), Right 80.0),
+    (Multi (Plus (Num 78) (Num 13)) (Num 0), Right 0.0),
+    (Multi (Plus (Num 10) (Num 15)) (Num 4), Right 100.0),
+    (Div (Num 0) (Num 87), Right 0.0),
+    (Div (Num 1) (Num 2), Right 0.5),
+    (Div (Num 34) (Num 0), Left DivisionByZero),
+    (Power (Num 3) (Num 2), Right 9.0),
+    (Power (Num 98) (Num 0), Right 1.0)
+  ]
 
 test :: Expr -> Either Error Double -> IO ()
 test expr expected =
-    let actual = eval expr in 
-    unless (expected == actual) $ describeFailure actual
+  let actual = eval expr
+   in unless (expected == actual) $ describeFailure actual
   where
     describeFailure actual =
       printf "eval (%s) should be %s but it was %s" (show expr) (show expected) (show actual)
@@ -36,4 +103,4 @@ test expr expected =
 main :: IO ()
 main = do
   mapM_ (uncurry test) cases
-  putStrLn "Done" 
+  putStrLn "Done"
