9b7f9b0b3ac4869a99a74063f1221cb9746e4a33
Yassine Founounou <Yassine.Founounou@acronis.com>
2024-10-01 15:22:44 +0200

    Founounou HW03

--- Main.hs
+++ Main.hs
@@ -14,7 +14,7 @@ data Expr
 
 instance Show Expr where
   show (NumExpr a) = show a
-  show (SqrtExpr a) = "(" ++ show a ++ ")"
+  show (SqrtExpr a) = "sqrt(" ++ show a ++ ")"
   show (AddExpr a b) = "(" ++ show a ++ "+" ++ show b ++ ")"
   show (SubExpr a b) = "(" ++ show a ++ "-" ++ show b ++ ")"
   show (MulExpr a b) = "(" ++ show a ++ "*" ++ show b ++ ")"
@@ -24,11 +24,11 @@ instance Show Expr where
 instance Eq Expr where
   NumExpr a == NumExpr b = a == b
   SqrtExpr a == SqrtExpr b = a == b
-  AddExpr a b == AddExpr c d = a==c && b==d
-  SubExpr a b == SubExpr c d = a==c && b==d
-  MulExpr a b == MulExpr c d = a==c && b==d
-  DivExpr a b == DivExpr c d = a==c && b==d
-  PowerExpr a b == PowerExpr c d = a==c && b==d
+  AddExpr a b == AddExpr c d = a == c && b == d
+  SubExpr a b == SubExpr c d = a == c && b == d
+  MulExpr a b == MulExpr c d = a == c && b == d
+  DivExpr a b == DivExpr c d = a == c && b == d
+  PowerExpr a b == PowerExpr c d = a == c && b == d
   _ == _ = False
 
 data Error
@@ -36,11 +36,11 @@ data Error
   | NegativeSqrtNum Expr
 
 instance Show Error where
-  show (DivByZero a b) = "Division by zero error: " ++ show a ++ "/" ++ show b
-  show (NegativeSqrtNum s) = "Cannot take square root of negative number" ++ show s
+  show (DivByZero a b) = "Division by zero error: " ++ show a ++ " / " ++ show b
+  show (NegativeSqrtNum s) = "Cannot take square root of negative number: " ++ show s
 
 instance Eq Error where
-  DivByZero _ _== DivByZero _ _ = True
+  DivByZero _ _ == DivByZero _ _ = True
   NegativeSqrtNum _ == NegativeSqrtNum _ = True
   _ == _ = False
 
@@ -64,17 +64,17 @@ eval (PowerExpr a b) = evalHelper (**) a b
 
 eval (DivExpr a b) = 
     case (eval a, eval b) of
-        (Right a, Right b) 
-            | b == 0 -> Left (DivByZero (NumExpr a) (NumExpr b))
-            | otherwise -> Right (a / b)
+        (Right aVal, Right bVal) 
+            | bVal == 0 -> Left (DivByZero a b)
+            | otherwise -> Right (aVal / bVal)
         (Left err, _) -> Left err
         (_, Left err) -> Left err
 
 eval (SqrtExpr x) = 
     case eval x of
-        Right x 
-          | x >= 0 -> Right (sqrt x)
-          | otherwise -> Left (NegativeSqrtNum (NumExpr x))
+        Right xVal 
+          | xVal >= 0 -> Right (sqrt xVal)
+          | otherwise -> Left (NegativeSqrtNum x)
         Left err -> Left err
 
 cases :: [(Expr, Either Error Double)]
@@ -89,6 +89,7 @@ cases =
   , (SqrtExpr (NumExpr (-9)), Left (NegativeSqrtNum (NumExpr (-9))))
   , (PowerExpr (NumExpr 2) (NumExpr 3), Right 8)
   , (AddExpr (MulExpr (NumExpr 2) (NumExpr 3)) (NumExpr 4), Right 10)
+  , (DivExpr (NumExpr 10) (NumExpr (1 - 1)), Left (DivByZero (NumExpr 10) (NumExpr (1 - 1))))
   ]
 
 test :: Expr -> Either Error Double -> IO ()
