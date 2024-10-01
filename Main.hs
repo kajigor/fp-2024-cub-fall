b59a8ee3b322e2ba7296716322ab168101c0f416
Ana Dobrescu <adobrescu@constructor.university>
2024-10-01 21:46:43 +0200

    changes in main

--- Main.hs
+++ Main.hs
@@ -15,13 +15,13 @@ data Expr
   | Power Expr Expr
 
 instance Show Expr where
-  show (Num a) = printf "Number: %s" (show a)
-  show (Sqrt a) = printf "Square root: %s" (show a)
-  show (Minus a b) = printf "Subtraction: %s - %s" (show a) (show b)
-  show (Plus a b) = printf "Addition: %s + %s" (show a) (show b)
-  show (Multi a b) = printf "Multiplication: %s" (show a) (show b)
-  show (Div a b) = printf "Division: %s / %s" (show a) (show b)
-  show (Power a b) = printf "Power: %s ^ %s" (show a) (show b)
+  show (Num a) = show a
+  show (Sqrt a) = printf "sqrt %s" (show a)
+  show (Minus a b) = printf "%s - %s" (show a) (show b)
+  show (Plus a b) = printf "%s + %s" (show a) (show b)
+  show (Multi a b) = printf "%s" (show a) (show b)
+  show (Div a b) = printf "%s / %s" (show a) (show b)
+  show (Power a b) = printf "%s ^ %s" (show a) (show b)
 
 instance Eq Expr where
   (==) :: Expr -> Expr -> Bool
@@ -35,23 +35,23 @@ instance Eq Expr where
   _ == _ = False
 
 data Error
-  = DivisionByZero
-  | SqrtOfNegNumber
+  = DivisionByZero Expr Expr
+  | SqrtOfNegNumber Expr
 
 instance Show Error where
-  show DivisionByZero = "Error: tried to do devision by zero"
-  show SqrtOfNegNumber = "Error: tried to find the square root of a negative number"
+  show (DivisionByZero a b) = printf "Error: tried to do devision by zero: %s / %s" (show a) (show b)
+  show (SqrtOfNegNumber a) = printf "Error: tried to find the square root of a negative number: sqrt %s" (show a)
 
 instance Eq Error where
-  DivisionByZero == DivisionByZero = True
-  SqrtOfNegNumber == SqrtOfNegNumber = True
+  DivisionByZero _ _ == DivisionByZero _ _ = True
+  SqrtOfNegNumber _ == SqrtOfNegNumber _ = True
   _ == _ = False
 
 eval :: Expr -> Either Error Double
 eval (Num a) = Right a
 eval (Sqrt a) = do
   val <- eval a
-  if val >= 0 then Right (sqrt val) else Left SqrtOfNegNumber
+  if val >= 0 then Right (sqrt val) else Left (SqrtOfNegNumber a)
 eval (Minus a b) = do
   val1 <- eval a
   val2 <- eval b
@@ -68,7 +68,7 @@ eval (Div a b) = do
   val1 <- eval a
   val2 <- eval b
   if val2 == 0
-    then Left DivisionByZero
+    then Left (DivisionByZero a b)
     else Right (val1 / val2)
 eval (Power a b) = do
   val1 <- eval a
@@ -80,14 +80,14 @@ cases =
   [ (Num 10, Right 10.0),
     (Num (-1), Right (-1.0)),
     (Sqrt (Num 25), Right 5.0),
-    (Sqrt (Num (-1)), Left SqrtOfNegNumber),
+    (Sqrt (Num (-1)), Left (SqrtOfNegNumber (Num (-1)))),
     (Minus (Num 78) (Num 8), Right 70.0),
     (Plus (Num 67) (Num 13), Right 80.0),
     (Multi (Plus (Num 78) (Num 13)) (Num 0), Right 0.0),
     (Multi (Plus (Num 10) (Num 15)) (Num 4), Right 100.0),
     (Div (Num 0) (Num 87), Right 0.0),
     (Div (Num 1) (Num 2), Right 0.5),
-    (Div (Num 34) (Num 0), Left DivisionByZero),
+    (Div (Num 34) (Num 0), Left (DivisionByZero (Num 34) (Num 0))),
     (Power (Num 3) (Num 2), Right 9.0),
     (Power (Num 98) (Num 0), Right 1.0)
   ]
