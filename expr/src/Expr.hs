7d76977ac9e9983ec407efe803587ef886474f4b
Ana Dobrescu <adobrescu@constructor.university>
2024-10-09 01:54:20 +0200

    expr.hs

--- expr/src/Expr.hs
+++ expr/src/Expr.hs
@@ -1,50 +1,37 @@
-module Expr where
+module Expr (Expr (..)) where
 
-import qualified Data.Map.Strict as M
-import GHC.Num (integerToInt)
+import Text.Printf (printf)
 
-data Expr = Lit Int | Plus Expr Expr | Var String
+data Expr
+  = Num Double
+  | Sqrt Expr
+  | Minus Expr Expr
+  | Plus Expr Expr
+  | Multi Expr Expr
+  | Div Expr Expr
+  | Power Expr Expr
+  | Var String
+  | Let String Expr Expr
 
 instance Show Expr where
-  show (Lit n) = show n
-  show (Plus x y) = '(' : show x ++ '+' : show y ++ ")"
-  show (Var v) = v
+  show (Num a) = show a
+  show (Sqrt a) = printf "(sqrt %s)" (show a)
+  show (Minus a b) = printf "(%s - %s)" (show a) (show b)
+  show (Plus a b) = printf "(%s + %s)" (show a) (show b)
+  show (Multi a b) = printf "(%s * %s)" (show a) (show b)
+  show (Div a b) = printf "(%s / %s)" (show a) (show b)
+  show (Power a b) = printf "(%s ^ %s)" (show a) (show b)
+  show (Var a) = show a
+  show (Let a b c) = printf "let %s = %s in %s" (show a) (show b) (show c)
 
-instance Num Expr where
-  (+) = Plus
-  (*) = undefined
-  abs = undefined
-  signum = undefined
-  fromInteger = Lit . integerToInt
-  negate = undefined
-
-eval :: M.Map String Expr -> Expr -> Maybe Int
-eval _ (Lit n) = Just n
-eval state (Plus x y) =
-  case (eval state x, eval state y) of
-    (Just x, Just y) -> Just $ x + y
-    _ -> Nothing
-eval state (Var v) = do
-  case M.lookup v state of
-    Just v -> eval state v
-    Nothing -> Nothing
-
-run :: Expr -> M.Map String Expr -> IO ()
-run expr state = do
-  print expr
-  print state
-  print (eval state expr)
-  putStrLn ""
-
-main = do
-  let expr1 = Var "x"
-  let expr2 = Plus (Lit 2) (Lit 2)
-  let expr3 = Plus (Var "x") (Lit 1)
-  let state1 = M.fromList [("x", Lit 42), ("y", Lit 13)]
-  let state2 = M.empty
-  run expr1 state1
-  run expr2 state1
-  run expr3 state1
-  run expr1 state2
-  run expr2 state2
-  run expr3 state2
+instance Eq Expr where
+  Num a == Num b = a == b
+  Sqrt a == Sqrt b = a == b
+  Minus a b == Minus c d = a == c && b == d
+  Plus a b == Plus c d = a == c && b == d
+  Multi a b == Multi c d = a == c && b == d
+  Div a b == Div c d = a == c && b == d
+  Power a b == Power c d = a == c && b == d
+  Var a == Var b = a == b
+  Let a b c == Let d e f = a == d && b == e && c == f
+  _ == _ = False
