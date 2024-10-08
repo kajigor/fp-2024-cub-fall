e4a2eefc40d89b78387fc903a0ea14b4ceab4e97
georgiiishopovv <131063161+georgiiishopovv@users.noreply.github.com>
2024-10-09 01:59:46 +0200

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -20,28 +20,23 @@ instance Show Operator where
 data Expr = Num Double 
      | Sqrt Expr 
      | CompExpr Operator Expr Expr
+     deriving (Eq)
 
 instance Show Expr where
     show (Num a) = show a
     show (Sqrt a) = printf "sqrt(%s)" (show a)
     show (CompExpr op a b) = printf "(%s %s %s)" (show a) (show op) (show b)
     
-instance Eq Expr where
-  Num a == Num b = a == b
-  Sqrt a == Sqrt b = a == b
-  CompExpr op a b == CompExpr op' a' b' = op == op' && a == a' && b == b
-  _ == _ = False
-
-data Error = NegativeSqrt
-     | ZeroDiv
+data Error = NegativeSqrt Expr
+     | ZeroDiv Expr
 
 instance Show Error where
-  show NegativeSqrt = printf "Error: Square root of a negative number"
-  show ZeroDiv = printf "Error: Division by 0 is not possible"
+  show (NegativeSqrt expr) = printf "Error: Square root of a negative number in expression: %s" (show expr)
+  show (ZeroDiv expr) = printf "Error: Division by 0 is not possible in expression: %s" (show expr)
 
 instance Eq Error where
-  NegativeSqrt == NegativeSqrt = True
-  ZeroDiv == ZeroDiv = True
+  NegativeSqrt expr1 == NegativeSqrt expr2 = expr1 == expr2
+  ZeroDiv expr1 == ZeroDiv expr2 = expr1 == expr2
   _ == _ = False
 
 eval :: Expr -> Either Error Double
@@ -50,7 +45,7 @@ eval (Sqrt expr) =
    case eval expr of
       Right x
          | x >= 0 -> Right (sqrt x)
-         | otherwise -> Left NegativeSqrt
+         | otherwise -> Left (NegativeSqrt expr)
       Left err -> Left err
 eval (CompExpr op a b) = 
    case (eval a, eval b) of
@@ -61,9 +56,14 @@ eval (CompExpr op a b) =
             Mult -> Right (val1 * val2)
             Div  -> 
                if val2 == 0 
-               then Left ZeroDiv
+               then Left (ZeroDiv (CompExpr Div a b))
                else Right (val1 / val2)
-            Pow -> Right (val1 ** val2)
+            Pow ->
+               if val1 == 0 && val2 < 0
+               then Left (ZeroDiv (CompExpr Pow a b))
+               else if val1 < 0 && (val2 /= fromInteger (round val2))
+               then Left (NegativeSqrt (CompExpr Pow a b))
+               else Right (val1 ** val2)
       (Left err, _) -> Left err
       (_, Left err) -> Left err                         
 
@@ -71,18 +71,20 @@ cases :: [(Expr, Either Error Double)]
 cases = 
   [ (Num 3, Right 3.0)
   , (Sqrt (Num 9), Right 3.0)
-  , (Sqrt (Num (-9)), Left NegativeSqrt)
+  , (Sqrt (Num (-9)), Left (NegativeSqrt (Num (-9))))
   , (CompExpr Add (Num 3) (Num 7), Right 10.0)
   , (CompExpr Sub (Num 7) (Num 3), Right 4.0)
   , (CompExpr Sub (Num 3) (Num 7), Right (-4.0))
   , (CompExpr Mult (Num 2) (Num 10), Right 20.0)
   , (CompExpr Div (Num 9) (Num 3), Right 3.0)
-  , (CompExpr Div (Num 3) (Num 0), Left ZeroDiv)
+  , (CompExpr Div (Num 3) (Num 0), Left (ZeroDiv (CompExpr Div (Num 3) (Num 0))))
   , (CompExpr Pow (Num 3) (Num 3), Right 27.0)
   , (Sqrt (CompExpr Sub (Num 7) (Num 3)), Right 2.0)
-  , (Sqrt (CompExpr Sub (Num 3) (Num 7)), Left NegativeSqrt)
+  , (Sqrt (CompExpr Sub (Num 3) (Num 7)), Left (NegativeSqrt (CompExpr Sub (Num 3) (Num 7))))
   , (CompExpr Add (CompExpr Mult (Num 2) (Num 10)) (CompExpr Sub (Num 10) (Num 2)), Right 28.0) 
-  , (CompExpr Div (CompExpr Mult (Num 2) (Num 10)) (CompExpr Sub (Num 10) (Num 10)), Left ZeroDiv)
+  , (CompExpr Div (CompExpr Mult (Num 2) (Num 10)) (CompExpr Sub (Num 10) (Num 10)), Left (ZeroDiv (CompExpr Div (CompExpr Mult (Num 2) (Num 10)) (CompExpr Sub (Num 10) (Num 10)))))
+  , (CompExpr Pow (Num 0) (Num (-4)), Left (ZeroDiv (CompExpr Pow (Num 0) (Num (-4)))))
+  , (CompExpr Pow (Num (-10)) (Num 0.5), Left (NegativeSqrt (CompExpr Pow (Num (-10)) (Num 0.5))))
   ]
 
 test :: Expr -> Either Error Double -> IO ()
