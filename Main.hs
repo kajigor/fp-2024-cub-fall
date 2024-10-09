f5b8f6e5a537a9f0995b461951e4b9616cec7687
darko1400 <145579698+darko1400@users.noreply.github.com>
2024-10-09 18:16:15 +0200

    Fix HW03

--- Main.hs
+++ Main.hs
@@ -12,48 +12,27 @@ data Expr
   | Mul Expr Expr
   | Div Expr Expr
   | Pow Expr Expr
-
-instance Show Expr where
-  show (Num x) = show x
-  show (Sqrt e) = "sqrt(" ++ show e ++ ")"
-  show (Add e1 e2) = "(" ++ show e1 ++ " + " ++ show e2 ++ ")"
-  show (Sub e1 e2) = "(" ++ show e1 ++ " - " ++ show e2 ++ ")"
-  show (Mul e1 e2) = "(" ++ show e1 ++ " * " ++ show e2 ++ ")"
-  show (Div e1 e2) = "(" ++ show e1 ++ " / " ++ show e2 ++ ")"
-  show (Pow e1 e2) = "(" ++ show e1 ++ " ^ " ++ show e2 ++ ")"
-
-instance Eq Expr where
-  (Num x) == (Num y) = x == y
-  (Sqrt e1) == (Sqrt e2) = e1 == e2
-  (Add e1 e2) == (Add f1 f2) = e1 == f1 && e2 == f2
-  (Sub e1 e2) == (Sub f1 f2) = e1 == f1 && e2 == f2
-  (Mul e1 e2) == (Mul f1 f2) = e1 == f1 && e2 == f2
-  (Div e1 e2) == (Div f1 f2) = e1 == f1 && e2 == f2
-  (Pow e1 e2) == (Pow f1 f2) = e1 == f1 && e2 == f2
-  _ == _ = False
+  deriving (Show, Eq)
 
 data Error
-  = DivByZero
-  | NegativeSqrt
+  = DivByZero Expr
+  | NegativeSqrt Expr
+  | InvalidPow Expr
   | OtherError String
+  deriving (Eq)
 
 instance Show Error where
-  show DivByZero = "Division by zero"
-  show NegativeSqrt = "Square root of a negative number"
+  show (DivByZero expr) = "Division by zero in: " ++ show expr
+  show (NegativeSqrt expr) = "Square root of a negative number in: " ++ show expr
+  show (InvalidPow expr) = "Invalid power operation in: " ++ show expr
   show (OtherError msg) = "Error: " ++ msg
-
-instance Eq Error where
-  DivByZero == DivByZero = True
-  NegativeSqrt == NegativeSqrt = True
-  (OtherError msg1) == (OtherError msg2) = msg1 == msg2
-  _ == _ = False
 
 eval :: Expr -> Either Error Double
 eval (Num x) = Right x
 eval (Sqrt e) = do
   x <- eval e
   if x < 0
-    then Left NegativeSqrt
+    then Left (NegativeSqrt (Sqrt e))
     else Right (sqrt x)
 eval (Add e1 e2) = (+) <$> eval e1 <*> eval e2
 eval (Sub e1 e2) = (-) <$> eval e1 <*> eval e2
@@ -61,18 +40,31 @@ eval (Mul e1 e2) = (*) <$> eval e1 <*> eval e2
 eval (Div e1 e2) = do
   x <- eval e2
   if x == 0
-    then Left DivByZero
+    then Left (DivByZero (Div e1 e2))
     else (/) <$> eval e1 <*> Right x
-eval (Pow e1 e2) = (**) <$> eval e1 <*> eval e2
+eval (Pow e1 e2) = do
+  base <- eval e1
+  exp <- eval e2
+  if base < 0 && exp /= fromInteger (round exp)
+    then Left (InvalidPow (Pow e1 e2))
+    else Right (base ** exp)
 
 cases :: [(Expr, Either Error Double)]
 cases =
   [ (Num 4, Right 4)
   , (Sqrt (Num 4), Right 2)
-  , (Sqrt (Num (-4)), Left NegativeSqrt)
+  , (Sqrt (Num (-4)), Left (NegativeSqrt (Sqrt (Num (-4)))))
   , (Add (Num 1) (Num 2), Right 3)
-  , (Div (Num 4) (Num 0), Left DivByZero)
+  , (Sub (Num 5) (Num 3), Right 2)
+  , (Mul (Num 2) (Num 3), Right 6)
+  , (Div (Num 6) (Num 2), Right 3)
+  , (Div (Num 4) (Num 0), Left (DivByZero (Div (Num 4) (Num 0))))
   , (Pow (Num 2) (Num 3), Right 8)
+  , (Pow (Num (-2)) (Num 0.5), Left (InvalidPow (Pow (Num (-2)) (Num 0.5))))
+  , (Div (Mul (Add (Num 4) (Num 5)) (Sub (Sqrt (Num 16)) (Num 2))) (Num 2), Right 9)
+  , (Pow (Num 2) (Num 10), Right 1024)
+  , (Sub (Num 3) (Num 5), Right (-2))
+  , (Pow (Num (-8)) (Div (Num 1) (Num 3)), Left (InvalidPow (Pow (Num (-8)) (Div (Num 1) (Num 3)))))
   ]
 
 test :: Expr -> Either Error Double -> IO ()
