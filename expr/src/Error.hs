3a312254bb9b4243621f82515402c02bc92fee78
georgiiishopovv <131063161+georgiiishopovv@users.noreply.github.com>
2024-10-10 16:25:06 +0200

    Update Error.hs

--- expr/src/Error.hs
+++ expr/src/Error.hs
@@ -1,15 +1,22 @@
-module Error(Error(..)) where
+module Error where
 
-import qualified Expr as e
+import Control.Monad (unless)
+import qualified Data.Map.Strict as M
+import Text.Printf (printf)
+
+import Expr
     
-data Error = NegativeSqrt e.Expr
-     | ZeroDiv e.Expr
+data Error = NegativeSqrt Expr
+     | ZeroDiv Expr
+     | Unbounded String
 
 instance Show Error where
   show (NegativeSqrt expr) = printf "Error: Square root of a negative number in expression: %s" (show expr)
   show (ZeroDiv expr) = printf "Error: Division by 0 is not possible in expression: %s" (show expr)
+  show (Unbounded var) = printf "Unbouded variable: %s" (show var)
 
 instance Eq Error where
   NegativeSqrt expr1 == NegativeSqrt expr2 = expr1 == expr2
-  ZeroDiv expr1 == ZeroDiv expr2 = expr1 == expr2
+  ZeroDiv expr1 == ZeroDiv expr2 = expr1 == expr2s
+  Unbounded var1 = Unbounded var2 = var1 == var2
   _ == _ = False
