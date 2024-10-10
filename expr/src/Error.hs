0b33c75909b77fe6016e013efb01ddb90126541d
georgiiishopovv <131063161+georgiiishopovv@users.noreply.github.com>
2024-10-10 16:38:57 +0200

    Update Error.hs

--- expr/src/Error.hs
+++ expr/src/Error.hs
@@ -9,14 +9,9 @@ import Expr
 data Error = NegativeSqrt Expr
      | ZeroDiv Expr
      | Unbounded String
+     deriving(Eq)
 
 instance Show Error where
   show (NegativeSqrt expr) = printf "Error: Square root of a negative number in expression: %s" (show expr)
   show (ZeroDiv expr) = printf "Error: Division by 0 is not possible in expression: %s" (show expr)
   show (Unbounded var) = printf "Unbouded variable: %s" (show var)
-
-instance Eq Error where
-  NegativeSqrt expr1 == NegativeSqrt expr2 = expr1 == expr2
-  ZeroDiv expr1 == ZeroDiv expr2 = expr1 == expr2s
-  Unbounded var1 = Unbounded var2 = var1 == var2
-  _ == _ = False
