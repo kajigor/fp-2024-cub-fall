63204edc9f8a8ac60b704c07c740044cce1e0281
georgiiishopovv <131063161+georgiiishopovv@users.noreply.github.com>
2024-10-10 16:24:41 +0200

    Update Expr.hs

--- expr/src/Expr.hs
+++ expr/src/Expr.hs
@@ -4,6 +4,8 @@ import Control.Monad (unless)
 import qualified Data.Map.Strict as M
 import Text.Printf (printf)
 
+import Interpreter
+
 data Operator = Add 
      | Sub 
      | Mult 
@@ -30,24 +32,4 @@ instance Show Expr where
     show (Sqrt a) = printf "sqrt(%s)" (show a)
     show (CompExpr op a b) = printf "(%s %s %s)" (show a) (show op) (show b)
     show (Var str) = show str
-    show (Let str a b) = prinf "let %s = %s in %s" (show str) (show a) (show b)
-
-run :: Expr -> M.Map String Expr -> IO ()
-run expr state = do
-  print expr
-  print state
-  print (eval state expr)
-  putStrLn ""
-
-main = do
-  let expr1 = Var "x"
-  let expr2 = CompExpr Add (Num 2) (Num 2)
-  let expr3 = CompExpr Add (Var "x") (Num 1)
-  let state1 = M.fromList [("x", Num 42), ("y", Num 13)]
-  let state2 = M.empty
-  run expr1 state1
-  run expr2 state1
-  run expr3 state1
-  run expr1 state2
-  run expr2 state2
-  run expr3 state2
+    show (Let str a b) = printf "let %s = %s in %s" (show str) (show a) (show b)
