39842bec43d7d104718dbed85ae19f84c6252b00
Borsos Matheas-Roland <122999210+matyborsos@users.noreply.github.com>
2024-10-22 18:21:31 +0200

    HW05

--- monads/src/HW/Eval.hs
+++ monads/src/HW/Eval.hs
@@ -4,10 +4,11 @@ import qualified Data.Map as M
 import State.MyState
 import HW.StackMachine
 
+
 -- Possible errors during evaluation
 data Error v
   = StackUnderflow (StackInstr v) -- Not enough numbers on the stack to process the instruction
-  | VarUndefined String           -- The variable is not defined in the environment 
+  | VarUndefined v                -- The variable is not defined in the environment 
   | StackNotExhausted Stack       -- After the program has finished evaluation, the stack is not a singleton
   deriving (Show, Eq)
 
@@ -20,7 +21,7 @@ data MachineState v = MachineState
   { getStack :: Stack,
     getEnv :: Env v
   }
-  deriving (Show)
+  deriving (Show, Eq)
 
 -- Run the compiled program on an empty stack and environment
 initialState :: MachineState String
@@ -29,8 +30,51 @@ initialState = MachineState [] M.empty
 -- Execute a single instruction. 
 -- Successful evaluation does not produce any useful result: only the effect of modifying state matters. 
 execInstr :: (Ord v, Show v) => StackInstr v -> MyState (MachineState v) (Either (Error v) ())
-execInstr = undefined 
+execInstr (PushNum n) = do
+  MachineState stack env <- get
+  put $ MachineState (n:stack) env
+  return $ Right ()
+
+execInstr (StoreVar v) = do
+  MachineState stack env <- get
+  case stack of
+    [] -> return $ Left $ StackUnderflow (StoreVar v)
+    (x:xs) -> do
+      put $ MachineState xs (M.insert v x env)
+      return $ Right ()
+
+execInstr Add = do
+  MachineState stack env <- get
+  case stack of
+    (x:y:xs) -> do
+      let result = x + y
+      put $ MachineState (result:xs) env
+      return $ Right ()
+    _ -> return $ Left $ StackUnderflow Add
+
+execInstr (PushVar v) = do
+  MachineState stack env <- get
+  case M.lookup v env of
+    Just x -> do
+      put $ MachineState (x:stack) env
+      return $ Right ()
+    Nothing -> return $ Left $ VarUndefined v
 
 -- Execute a list of instructions starting from the given state. 
 execProgram :: (Ord v, Show v) => StackProgram v -> MachineState v -> Either (Error v) (MachineState v)
-execProgram = undefined 
+execProgram [] state = Right state 
+execProgram (instr:instrs) state = 
+    let (finalState, result) = runMyState (execInstr instr) state
+    in case result of
+        Left err -> Left err
+        Right () -> 
+            case execProgram instrs finalState of
+                Left err -> Left err
+                Right newState -> 
+                    if length (getStack newState) /= 1
+                        then Left $ StackNotExhausted (getStack newState)
+                        else Right newState
+
+
+
+
--- monads/test/Spec.hs
+++ monads/test/Spec.hs
@@ -1,2 +1,87 @@
+import Test.Tasty
+import Test.Tasty.HUnit
+import qualified Data.Map.Strict as M
+import qualified HW.Eval
+import qualified Expr
+import qualified HW.Compiler
+import qualified HW.StackMachine
+import State.MyState (runMyState)
+
+
+-- Test suite for compile function
+testCompile :: TestTree
+testCompile = testGroup "compile tests"
+  [ testCase "compile simple let expression" $
+      let expr = Expr.Let "x" (Expr.Num 5) (Expr.Plus (Expr.Var "x") (Expr.Num 10))
+          expected = [ HW.StackMachine.PushNum 5, HW.StackMachine.StoreVar "x", HW.StackMachine.PushVar "x", HW.StackMachine.PushNum 10, HW.StackMachine.Add]
+      in HW.Compiler.compile expr @?= expected
+
+  , testCase "compile nested let expression" $
+      let expr = Expr.Let "x" (Expr.Num 13)
+                    (Expr.Let "y" (Expr.Num 42)
+                      (Expr.Let "x" (Expr.Plus (Expr.Var "x") (Expr.Var "y"))
+                        (Expr.Plus (Expr.Var "x") (Expr.Var "y"))))
+          expected = [HW.StackMachine.PushNum 13, HW.StackMachine.StoreVar "x", 
+                      HW.StackMachine.PushNum 42, HW.StackMachine.StoreVar "y", 
+                      HW.StackMachine.PushVar "x", HW.StackMachine.PushVar "y", HW.StackMachine.Add, HW.StackMachine.StoreVar "x", 
+                      HW.StackMachine.PushVar "x", HW.StackMachine.PushVar "y", HW.StackMachine.Add]
+      in HW.Compiler.compile expr @?= expected
+  ]
+
+-- Test suite for execInstr function
+testExecInstr :: TestTree
+testExecInstr = testGroup "execInstr tests"
+  [ testCase "PushNum 5" $ do
+      let (finalState1, results1) = runMyState (HW.Eval.execInstr (HW.StackMachine.PushNum 5)) HW.Eval.initialState
+      assertEqual "Stack should contain 5" (Right ()) results1
+      assertEqual "Final stack should be [5]" [5] (HW.Eval.getStack finalState1)
+
+  , testCase "Add should fail due to stack underflow" $ do
+      let (finalState2, results2) = runMyState (HW.Eval.execInstr HW.StackMachine.Add) HW.Eval.initialState
+      assertEqual "Add should fail due to stack underflow" (Left (HW.Eval.StackUnderflow HW.StackMachine.Add)) results2
+
+  , testCase "PushVar undefined" $ do
+      let (finalState3, results3) = runMyState (HW.Eval.execInstr (HW.StackMachine.PushVar "x")) HW.Eval.initialState
+      assertEqual "PushVar should fail due to undefined variable" (Left (HW.Eval.VarUndefined "x")) results3
+
+  , testCase "StoreVar with empty stack" $ do
+      let (finalState4, results4) = runMyState (HW.Eval.execInstr (HW.StackMachine.StoreVar "x")) HW.Eval.initialState
+      assertEqual "StoreVar should fail due to stack underflow" (Left (HW.Eval.StackUnderflow (HW.StackMachine.StoreVar "x"))) results4
+
+  , testCase "Add with insufficient values on the stack" $ do
+      let (finalState5, results5) = runMyState (HW.Eval.execInstr HW.StackMachine.Add) HW.Eval.initialState
+      assertEqual "Add should fail due to stack underflow" (Left (HW.Eval.StackUnderflow HW.StackMachine.Add)) results5
+  ]
+
+
+-- Test suite for execProgram function
+testExecProgram :: TestTree
+testExecProgram = testGroup "execProgram tests"
+  [ testCase "run exampleProgram" $
+      let program = [HW.StackMachine.PushNum 5, HW.StackMachine.PushNum 10, HW.StackMachine.Add, HW.StackMachine.StoreVar "x", HW.StackMachine.PushVar "x", HW.StackMachine.PushNum 2, HW.StackMachine.Add]
+      in HW.Eval.execProgram program HW.Eval.initialState @?= Right (HW.Eval.MachineState [17] (M.fromList [("x", 15)]))
+
+  , testCase "run underflowExpr" $
+      let program = [HW.StackMachine.PushNum 13, HW.StackMachine.Add]
+      in HW.Eval.execProgram program HW.Eval.initialState @?= Left (HW.Eval.StackUnderflow HW.StackMachine.Add)
+
+  , testCase "run undefVar program" $
+      let program = [HW.StackMachine.PushVar "x"]
+      in HW.Eval.execProgram program HW.Eval.initialState @?= Left (HW.Eval.VarUndefined "x")
+
+  , testCase "complex nested Expr.let program" $
+      let program = HW.Compiler.compile (Expr.Let "x" (Expr.Num 13)
+                              (Expr.Let "y" (Expr.Num 42)
+                                (Expr.Let "x" (Expr.Plus (Expr.Var "x") (Expr.Var "y"))
+                                  (Expr.Plus (Expr.Var "x") (Expr.Var "y")))))
+          expected = Right (HW.Eval.MachineState [97] (M.fromList [("x", 55), ("y", 42)]))
+      in HW.Eval.execProgram program HW.Eval.initialState @?= expected
+  ]
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain $ testGroup "Stack Machine Tests"
+  [ testCompile
+  , testExecInstr
+  , testExecProgram
+  ]
