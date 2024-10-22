8db2cf2e3d0600b5ab87510ca862e9aabb530e47
Ana Dobrescu <adobrescu@constructor.university>
2024-10-22 11:25:09 +0200

    lecture

--- monads/src/Reader/MyReader.hs
+++ monads/src/Reader/MyReader.hs
@@ -1,25 +1,26 @@
 {-# LANGUAGE InstanceSigs #-}
-module Reader.MyReader where 
 
-newtype MyReader r a = MyReader { runMyReader :: r -> a }
+module Reader.MyReader where
 
-ask :: MyReader r r 
-ask = undefined 
+newtype MyReader r a = MyReader {runMyReader :: r -> a}
 
-local :: (r -> r) -> MyReader r a -> MyReader r a 
-local f (MyReader g) = undefined 
+ask :: MyReader r r
+ask = MyReader id
 
-instance Functor (MyReader r) where 
-  fmap :: (a -> b) -> MyReader r a -> MyReader r b 
-  fmap f (MyReader g) = undefined 
+local :: (r -> r) -> MyReader r a -> MyReader r a
+local f (MyReader g) = MyReader (g . f)
 
-instance Applicative (MyReader r) where 
-  pure :: a -> MyReader r a 
-  pure = undefined 
+instance Functor (MyReader r) where
+  fmap :: (a -> b) -> MyReader r a -> MyReader r b
+  fmap f (MyReader g) = MyReader (f . g)
+
+instance Applicative (MyReader r) where
+  pure :: a -> MyReader r a
+  pure x = MyReader (const x)
 
   (<*>) :: MyReader r (a -> b) -> MyReader r a -> MyReader r b
-  MyReader f <*> MyReader x = undefined
+  MyReader f <*> MyReader x = MyReader (\r -> f r (x r))
 
-instance Monad (MyReader r) where 
+instance Monad (MyReader r) where
   (>>=) :: MyReader r a -> (a -> MyReader r b) -> MyReader r b
-  MyReader x >>= m = undefined
+  MyReader x >>= m = MyReader (\r -> runMyReader (m (x r)) r)
