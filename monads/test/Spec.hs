24a5d513bee050c1993026c884f77e7ab5de99cb
Serzhan Kenesbek <serzhan.kenesbek1@gmail.com>
2024-10-24 22:24:39 +0200

    Misunderstood what the Expr looked like

--- monads/test/Spec.hs
+++ monads/test/Spec.hs
@@ -20,49 +20,49 @@ tests = testGroup "Combined Compilation and Evaluation Tests"
 combinedTests :: TestTree
 combinedTests = testGroup "Combined Tests"
   [ testCase "combined test for nested let expressions with 13 and 42" $
-      let expr = LetExpr "x" (NumExpr 13) (
-                 LetExpr "y" (NumExpr 42) (
-                 LetExpr "x" (AddExpr (VarExpr "x") (VarExpr "y")) (
-                 AddExpr (VarExpr "x") (VarExpr "y"))))
+      let expr = Let "x" (Num 13) (
+                 Let "y" (Num 42) (
+                 Let "x" (Plus (Var "x") (Var "y")) (
+                 Plus (Var "x") (Var "y"))))
           compiled = compile expr
           result = execProgram compiled initialState
           expected = Right $ MachineState [97] []
       in result @?= expected
 
   , testCase "combined test for variable shadowing" $
-      let expr = LetExpr "x" (NumExpr 5) (
-                 LetExpr "x" (NumExpr 10) (
-                 AddExpr (VarExpr "x") (NumExpr 20)))
+      let expr = Let "x" (Num 5) (
+                 Let "x" (Num 10) (
+                 Plus (Var "x") (Num 20)))
           compiled = compile expr
           result = execProgram compiled initialState
           expected = Right $ MachineState [30] []
       in result @?= expected
 
   , testCase "combined test for basic addition" $
-      let expr = AddExpr (NumExpr 3) (NumExpr 4)
+      let expr = Plus (Num 3) (Num 4)
           compiled = compile expr
           result = execProgram compiled initialState
           expected = Right $ MachineState [7] []
       in result @?= expected
 
   , testCase "combined test for storing and retrieving a variable" $
-      let expr = LetExpr "x" (NumExpr 10) (AddExpr (VarExpr "x") (NumExpr 5))
+      let expr = Let "x" (Num 10) (Plus (Var "x") (Num 5))
           compiled = compile expr
           result = execProgram compiled initialState
           expected = Right $ MachineState [15] []
       in result @?= expected
 
-  , testCase "combined test for undefined variable." $
-      let expr = LetExpr "x" (NumExpr 10) (VarExpr "undefined")
+  , testCase "combined test for undefined variable" $
+      let expr = Let "x" (Num 10) (Var "undefined")
           compiled = compile expr
           result = execProgram compiled initialState
           expected = Left (VarUndefined "undefined")
       in result @?= expected
 
   , testCase "combined test for stack underflow on Add" $
-      let expr = LetExpr "x" (NumExpr 1) (
-                 LetExpr "y" (NumExpr 2) (
-                 AddExpr (VarExpr "x") (VarExpr "y")))
+      let expr = Let "x" (Num 1) (
+                 Let "y" (Num 2) (
+                 Plus (Var "x") (Var "y")))
           compiled = compile expr
           result = execProgram (compiled ++ [Add]) initialState
           expected = Left (StackUnderflow Add)
