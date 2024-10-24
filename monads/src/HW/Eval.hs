71733a01ca2e4b19c724609c34f0c46f64972012
darko1400 <145579698+darko1400@users.noreply.github.com>
2024-10-24 16:28:43 +0200

    Update Eval.hs

--- monads/src/HW/Eval.hs
+++ monads/src/HW/Eval.hs
@@ -29,8 +29,46 @@ initialState = MachineState [] M.empty
 -- Execute a single instruction. 
 -- Successful evaluation does not produce any useful result: only the effect of modifying state matters. 
 execInstr :: (Ord v, Show v) => StackInstr v -> MyState (MachineState v) (Either (Error v) ())
-execInstr = undefined 
+execInstr (PushNum n) = do
+  modify $ \state -> state { getStack = n : getStack state }
+  return (Right ())
+
+execInstr (PushVar var) = do
+  state <- get
+  case M.lookup var (getEnv state) of
+    Just value -> do
+      modify $ \s -> s { getStack = value : getStack s }
+      return (Right ())
+    Nothing -> return (Left (VarUndefined (show var))) 
+
+execInstr Add = do
+  state <- get
+  case getStack state of
+    (x:y:rest) -> do
+      modify $ \s -> s { getStack = (x + y) : rest }
+      return (Right ())
+    _ -> return (Left $ StackUnderflow Add)
+
+execInstr (StoreVar var) = do
+  state <- get
+  case getStack state of
+    (top:rest) -> do
+      put $ state { getStack = rest, getEnv = M.insert var top (getEnv state)}
+      return (Right ())
+    _ -> return (Left $ StackUnderflow (StoreVar var))
+
 
 -- Execute a list of instructions starting from the given state. 
 execProgram :: (Ord v, Show v) => StackProgram v -> MachineState v -> Either (Error v) (MachineState v)
-execProgram = undefined 
+execProgram program initialState =
+  let runInstructions [] state = 
+        case getStack state of
+          [singleResult] -> Right state 
+          stack -> Left $ StackNotExhausted stack  
+      runInstructions (instr:instrs) state = 
+        let result = execInstr instr
+            (tempState, execResult) = runMyState result state
+        in case execResult of
+             Left err -> Left err
+             Right () -> runInstructions instrs tempState
+  in runInstructions program initialState 
