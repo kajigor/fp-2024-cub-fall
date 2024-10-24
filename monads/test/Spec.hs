5b7f15255c80fa5d236aad10b980fbc9df4ec4a4
Serzhan Kenesbek <serzhan.kenesbek1@gmail.com>
2024-10-24 22:54:12 +0200

    Fix

--- monads/test/Spec.hs
+++ monads/test/Spec.hs
@@ -12,15 +12,10 @@ import Expr
 import qualified Data.Map as M
 
 main :: IO ()
-main = defaultMain tests
-
-tests :: TestTree
-tests = testGroup "Combined Compilation and Evaluation Tests"
-  [ combinedTests
-  ]
+main = defaultMain combinedTests
 
 combinedTests :: TestTree
-combinedTests = testGroup "Combined Tests"
+combinedTests = testGroup "Combined tests for eval and compiler"
   [ testCase "combined test for nested let expressions with 13 and 42" $
       let expr = Let "x" (Num 13) (
                  Let "y" (Num 42) (
@@ -28,7 +23,7 @@ combinedTests = testGroup "Combined Tests"
                  Plus (Var "x") (Var "y"))))
           compiled = compile expr
           result = execProgram compiled initialState
-          expected = Right $ MachineState [97] M.empty
+          expected = Right $ MachineState [97] (M.fromList [("x", 55), ("y", 42)])
       in result @?= expected
 
   , testCase "combined test for variable shadowing" $
@@ -37,7 +32,7 @@ combinedTests = testGroup "Combined Tests"
                  Plus (Var "x") (Num 20)))
           compiled = compile expr
           result = execProgram compiled initialState
-          expected = Right $ MachineState [30] M.empty
+          expected = Right $ MachineState [30] (M.fromList [("x", 10)])
       in result @?= expected
 
   , testCase "combined test for basic addition" $
@@ -51,7 +46,7 @@ combinedTests = testGroup "Combined Tests"
       let expr = Let "x" (Num 10) (Plus (Var "x") (Num 5))
           compiled = compile expr
           result = execProgram compiled initialState
-          expected = Right $ MachineState [15] M.empty
+          expected = Right $ MachineState [15] (M.fromList [("x", 10)])
       in result @?= expected
 
   , testCase "combined test for undefined variable" $
