2cda3d0e98f17a262536522b2737d73ca4ee7f0f
Ana Dobrescu <adobrescu@constructor.university>
2024-10-26 15:19:41 +0200

    changes in Eval.hs

--- monads/src/HW/Eval.hs
+++ monads/src/HW/Eval.hs
@@ -29,32 +29,28 @@ initialState = MachineState [] M.empty
 -- Execute a single instruction.
 -- Successful evaluation does not produce any useful result: only the effect of modifying state matters.
 execInstr :: (Ord v, Show v) => StackInstr v -> MyState (MachineState v) (Either (Error v) ())
-execInstr (PushNum x) = do
-  MachineState s env <- get
-  put (MachineState (x : s) env)
-  return (Right ())
+execInstr (PushNum n) =
+  Right <$> modify (\m -> m {getStack = n : getStack m})
 execInstr (PushVar x) = do
   MachineState s env <- get
   case M.lookup x env of
-    Just val -> do
-      put (MachineState (val : s) env)
-      return (Right ())
-    Nothing -> return (Left (VarUndefined (show x)))
+    Just val -> put (MachineState (val : s) env) >> pure (Right ())
+    Nothing -> pure (Left (VarUndefined (show x)))
 execInstr Add = do
   MachineState s env <- get
   case s of
     (x : y : rest) -> do
       let result = x + y
       put (MachineState (result : rest) env)
-      return (Right ())
-    _ -> return (Left (StackUnderflow Add))
+      pure (Right ())
+    _ -> pure (Left (StackUnderflow Add))
 execInstr (StoreVar v) = do
   MachineState s env <- get
   case s of
     (val : rest) -> do
       put (MachineState rest (M.insert v val env))
-      return (Right ())
-    _ -> return (Left (StackUnderflow (StoreVar v)))
+      pure (Right ())
+    _ -> pure (Left (StackUnderflow (StoreVar v)))
 
 -- Execute a list of instructions starting from the given state.
 execProgram :: (Ord v, Show v) => StackProgram v -> MachineState v -> Either (Error v) (MachineState v)
