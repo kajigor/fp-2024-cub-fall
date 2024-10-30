37b5756dc60e3b135779e33dddf0f85c19917fb0
abanoubseddik <abanoub.maged171@gmail.com>
2024-10-30 09:14:57 +0100

    MidTerm Done

--- midterm/app/Main.hs
+++ midterm/app/Main.hs
@@ -1,11 +1,23 @@
 module Main (main) where
 
-import Directory
+import Directory (buildTree, defaultDisplayTree, alphabetisedDisplayTree)
+import System.IO ()
 
 -- Implement a function that asks the user for the directory name and the sorting type, constructs the tree and then displays it accordingly.
 run :: IO () 
-run = undefined 
+run = do
+    putStrLn "Enter the directory path:"
+    dirPath <- getLine
+    putStrLn "Choose sorting type default display or alphabetised display:"
+    sortingChoice <- getLine
+
+    tree <- buildTree dirPath
+
+    case sortingChoice of
+        "1" -> putStrLn (defaultDisplayTree tree)
+        "2" -> putStrLn (alphabetisedDisplayTree tree)
+        _   -> putStrLn "Invalid choice. Please enter 1 or 2."
 
 -- Use the main function to demostrate how run works. 
 main :: IO ()
-main = undefined 
+main = run 
--- midterm/src/Directory.hs
+++ midterm/src/Directory.hs
@@ -1,17 +1,52 @@
 module Directory where
 
-import Tree
+import Tree()
+import Data.Set (Set)
+import qualified Data.Set as Set
+import System.Directory (doesDirectoryExist, listDirectory)
+import System.FilePath ((</>))
+import Data.List (sort)
+
 
 -- Define an appropriate type called `DirectoryTree` to represent the directory hierarchy
+data DirectoryTree
+  = DirNode FilePath (Set DirectoryTree)
+  | FileNode FilePath
+  deriving (Show, Eq)
 
 -- Create a tree that represents the hierarchy of files and subdirectories of the given directory.
 buildTree :: FilePath -> IO DirectoryTree
-buildTree = undefined 
+buildTree path = do
+    isDir <- doesDirectoryExist path
+    if isDir
+        then do
+            contents <- listDirectory path
+            subItems <- mapM (buildTree . (path </>)) contents
+            let subTreeSet = Set.fromList subItems
+            return (DirNode path subTreeSet)
+        else
+            return (FileNode path)
+
+
+displayHelper :: Int -> DirectoryTree -> [String]
+displayHelper indent (FileNode name) = [replicate (indent * 2) ' ' ++ name]
+displayHelper indent (DirNode name subItems) =
+    (replicate (indent * 2) ' ' ++ name) :
+    concatMap (displayHelper (indent + 1)) sortedSubItems
+  where
+    sortedSubItems = sort (Set.toList subItems)
+
+instance Ord DirectoryTree where
+    compare (FileNode name1) (FileNode name2) = compare name1 name2
+    compare (DirNode name1 _) (DirNode name2 _) = compare name1 name2
+    compare (FileNode _) (DirNode _ _) = GT
+    compare (DirNode _ _) (FileNode _) = LT
+
 
 -- Implement a function that displays the subdirectories of the current directory before the files in it. 
 defaultDisplayTree :: DirectoryTree -> String
-defaultDisplayTree = undefined 
+defaultDisplayTree = unlines . displayHelper 0
 
 -- Implement a function that displays the subdirectories and the files in it in the alphabet order. 
 alphabetisedDisplayTree :: DirectoryTree -> String
-alphabetisedDisplayTree = undefined 
+alphabetisedDisplayTree = unlines . displayHelper 0
