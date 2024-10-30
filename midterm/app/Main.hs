d696869866248e45ecf3880926a29a7f4f8d513a
imrun <turganovimrun@gmail.com>
2024-10-30 09:33:23 +0100

    not all done

--- midterm/app/Main.hs
+++ midterm/app/Main.hs
@@ -2,10 +2,19 @@ module Main (main) where
 
 import Directory
 
+
 -- Implement a function that asks the user for the directory name and the sorting type, constructs the tree and then displays it accordingly.
 run :: IO () 
-run = undefined 
+run = do
+    putStr "Enter directory path: "
+    hFlush stdout
+    dirPath <- getLine
+    putStrLn "Sort type (1 - alphabet, 0 - default)"
+    hFlush stdout
+    sortType <- getLine
+    tree <- buildTree dirPath
+    putStrLn $ defaultDisplayTree tree
 
 -- Use the main function to demostrate how run works. 
 main :: IO ()
-main = undefined 
+main = run
--- midterm/src/Directory.hs
+++ midterm/src/Directory.hs
@@ -2,15 +2,35 @@ module Directory where
 
 import Tree
 
+import qualified Data.Set as Set
+import System.Directory (doesDirectoryExist, listDirectory)
+import System.FilePath ((</>))
+import Control.Monad (forM)
 -- Define an appropriate type called `DirectoryTree` to represent the directory hierarchy
+
+data DirectoryTree
+    = Path FilePath
+    | Directory FilePath [DirectoryTree]
 
 -- Create a tree that represents the hierarchy of files and subdirectories of the given directory.
 buildTree :: FilePath -> IO DirectoryTree
-buildTree = undefined 
+buildTree path = do
+    isDir <- doesDirectoryExist path
+    if isDir
+        then do
+            contents <- listDirectory path
+            items <- forM contents $ \name -> buildTree (path </> name)
+            return $ Directory path items
+        else return $ Path path
+
 
 -- Implement a function that displays the subdirectories of the current directory before the files in it. 
 defaultDisplayTree :: DirectoryTree -> String
-defaultDisplayTree = undefined 
+defaultDisplayTree = outp ""
+  where
+    outp pr (Path name) = pr ++ name ++ "\n"
+    outp pr (Directory name contents) =
+        pr ++ name ++ "/\n" ++ concatMap (outp (pr ++ "    ")) contents
 
 -- Implement a function that displays the subdirectories and the files in it in the alphabet order. 
 alphabetisedDisplayTree :: DirectoryTree -> String
