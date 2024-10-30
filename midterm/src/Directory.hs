c11148869c2040a31561201d6c26bd2e94447855
Enrike Valentinov <evalentinov@constructor.university>
2024-10-30 09:31:22 +0100

    Enrike Valentinov commit

--- midterm/src/Directory.hs
+++ midterm/src/Directory.hs
@@ -1,12 +1,45 @@
 module Directory where
 
+import qualified Data.Set as Set
+import System.Directory (doesDirectoryExist, listDirectory)
+import System.FilePath ((</>))
 import Tree
 
 -- Define an appropriate type called `DirectoryTree` to represent the directory hierarchy
+data DirectoryEntry = 
+    File String
+    | Directory String
+    deriving (Show, Eq)
 
+instance Ord DirectoryEntry where
+    (File name1) `compare` (File name2) = name1 `compare` name2
+    (Directory name1) `compare` (Directory name2) = name1 `compare` name2
+    (Directory _) `compare` (File _) = LT
+    (File _) `compare` (Directory _) = GT
+
+type DirectoryTree = SetTree DirectoryEntry
 -- Create a tree that represents the hierarchy of files and subdirectories of the given directory.
 buildTree :: FilePath -> IO DirectoryTree
-buildTree = undefined 
+buildTree path = do
+    isDir <- doesDirectoryExist path
+    if isDir
+        then do
+            contents <- listDirectory path
+            entries <- mapM buildEntry contents
+            return $ Node (Directory (takeFileName path)) (Set.fromList entries)
+        else
+            error $ path ++ " is not a directory."
+  where
+    buildEntry :: FilePath -> IO (SetTree DirectoryEntry)
+    buildEntry name = do
+        let fullPath = path </> name
+        isDir <- doesDirectoryExist fullPath
+        if isDir
+            then buildTree fullPath
+            else return $ Leaf (File name)
+
+    takeFileName :: FilePath -> String
+    takeFileName = reverse . takeWhile (/= '/') . reverse
 
 -- Implement a function that displays the subdirectories of the current directory before the files in it. 
 defaultDisplayTree :: DirectoryTree -> String
--- midterm/src/Tree.hs
+++ midterm/src/Tree.hs
@@ -6,4 +6,16 @@ data SetTree a
   = Node a (Set.Set (SetTree a))
   | Leaf a 
 
-
+instance Eq a => Eq (SetTree a) where
+    (Leaf x) == (Leaf y) = x == y
+    (Node x xs) == (Node y ys) = x == y && xs == ys
+    _ == _ = False
+    
+instance Ord a => Ord (SetTree a) where
+    (Leaf x) `compare` (Leaf y) = x `compare` y
+    (Leaf x) `compare` (Node _ _) = LT
+    (Node _ _) `compare` (Leaf _) = GT
+    (Node x xs) `compare` (Node y ys) =
+        case x `compare` y of
+            EQ -> xs `compare` ys
+            other -> other
