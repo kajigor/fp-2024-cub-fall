7097652f60e303ec6839a56b5633729c4f43fec3
matyborsos <mborsos@constructor.university>
2024-11-05 11:24:06 +0100

    solved issues

--- monads/src/HW/Eval.hs
+++ monads/src/HW/Eval.hs
@@ -31,49 +31,40 @@ initialState = MachineState [] M.empty
 -- Successful evaluation does not produce any useful result: only the effect of modifying state matters. 
 execInstr :: (Ord v, Show v) => StackInstr v -> MyState (MachineState v) (Either (Error v) ())
 execInstr (PushNum n) = do
-  MachineState stack env <- get
-  put $ MachineState (n:stack) env
+  modify (\(MachineState stack env) -> MachineState (n:stack) env)
   return $ Right ()
 
-execInstr (StoreVar v) = do
-  MachineState stack env <- get
-  case stack of
-    [] -> return $ Left $ StackUnderflow (StoreVar v)
-    (x:xs) -> do
-      put $ MachineState xs (M.insert v x env)
-      return $ Right ()
-
 execInstr Add = do
-  MachineState stack env <- get
+  stack <- gets (\(MachineState stack _) -> stack)
   case stack of
     (x:y:xs) -> do
-      let result = x + y
-      put $ MachineState (result:xs) env
+      modify $ \(MachineState _ env) -> MachineState ((x + y):xs) env
       return $ Right ()
     _ -> return $ Left $ StackUnderflow Add
 
+execInstr (StoreVar var) = do
+  stack <- gets (\(MachineState stack _) -> stack)
+  case stack of
+    (x:xs) -> do
+      modify $ \(MachineState _ env) -> MachineState xs (M.insert var x env)
+      return $ Right ()
+    _ -> return $ Left $ StackUnderflow (StoreVar var)
+
 execInstr (PushVar v) = do
-  MachineState stack env <- get
+  env <- gets (\(MachineState _ env) -> env)
   case M.lookup v env of
     Just x -> do
-      put $ MachineState (x:stack) env
+      modify (\(MachineState stack env) -> MachineState (x:stack) env)
       return $ Right ()
     Nothing -> return $ Left $ VarUndefined v
 
 -- Execute a list of instructions starting from the given state. 
 execProgram :: (Ord v, Show v) => StackProgram v -> MachineState v -> Either (Error v) (MachineState v)
-execProgram [] state = Right state 
-execProgram (instr:instrs) state = 
-    let (finalState, result) = runMyState (execInstr instr) state
-    in case result of
-        Left err -> Left err
-        Right () -> 
-            case execProgram instrs finalState of
-                Left err -> Left err
-                Right newState -> 
-                    if length (getStack newState) /= 1
-                        then Left $ StackNotExhausted (getStack newState)
-                        else Right newState
+execProgram [] state = Right state
+execProgram (instr:instrs) state = do
+  case runMyState (execInstr instr) state of
+    (finalState, Left err) -> Left err
+    (finalState, Right ()) -> execProgram instrs finalState
 
 
 
