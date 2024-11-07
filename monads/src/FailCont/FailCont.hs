7209417915df407a7942e5a0b1f77fff4ac6992e
abanoubseddik <abanoub.maged171@gmail.com>
2024-11-07 19:49:19 +0100

    modified

--- monads/src/FailCont/FailCont.hs
+++ monads/src/FailCont/FailCont.hs
@@ -6,24 +6,26 @@ module FailCont.FailCont where
 newtype FailCont r e a = FailCont ((a -> r) -> (e -> r) -> r)
 
 toFailCont :: Either e a -> FailCont r e a
-toFailCont (Right a) = FailCont $ \OnSuccess _ -> OnSuccess a
-toFailCont (Left e) = FailCont $ \_ OnFailure -> OnFailure e
+toFailCont (Right a) = FailCont $ \onSuccess _ -> onSuccess a
+toFailCont (Left e) = FailCont $ \_ onFailure -> onFailure e
 
 evalFailCont :: FailCont (Either e a) e a -> Either e a
 evalFailCont (FailCont f) = f Right Left
 
 instance Functor (FailCont r e) where
-  fmap f (FailCont cont) = FailCont $ \OnSuccess OnFailure ->
-        cont (OnSuccess . f) OnFailure
+  fmap f (FailCont cont) = FailCont $ \onSuccess onFailure ->
+        cont (onSuccess . f) onFailure
 
 
 instance Applicative (FailCont r e) where
-  pure x = FailCont $ \OnSuccess _ ->OnSuccess x
-  (FailCont f) <*> (FailCont x) = FailCont $ \OnSuccess OnFailure ->
+  pure x = FailCont $ \onSuccess _ ->onSuccess x
+  (FailCont f) <*> (FailCont x) = FailCont $ \onSuccess onFailure ->
     f (\f' -> x (onSuccess . f') onFailure) onFailure
 
 
 instance Monad (FailCont r e) where
   return = pure
-    (FailCont x) >>= f = FailCont $ \onSuccess onFailure ->
-      x (\a -> let (FailCont y) = f a in y OnSuccess onFailure) OnFailure
+  (FailCont x) >>= f = FailCont $ \onSuccess onFailure ->
+    x (\a -> let (FailCont y) = f a in y onSuccess onFailure)
+      onFailure
+
