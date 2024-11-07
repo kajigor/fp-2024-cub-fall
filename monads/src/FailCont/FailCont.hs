105367ca2c3906744439f6ba06a616821df660c5
imrun <turganovimrun@gmail.com>
2024-11-07 23:18:05 +0100

    HW06

--- monads/src/FailCont/FailCont.hs
+++ monads/src/FailCont/FailCont.hs
@@ -1,18 +1,27 @@
-module FailCont.FailCont where 
+module FailCont.FailCont where
 
--- r is the result 
+-- r is the result
 -- e is the type of a failure
 -- a is the type of a success
-newtype FailCont r e a = undefined 
+newtype FailCont r e a = FailCont{ runFailCont ::( Either e a -> r) -> r}
 
 toFailCont :: Either e a -> FailCont r e a
-toFailCont = undefinedd 
+toFailCont x = FailCont $ \f -> f x
 
 evalFailCont :: FailCont (Either e a) e a -> Either e a
-evalFailCont = undefined 
+evalFailCont (FailCont cont) = cont id
 
 instance Functor (FailCont r e) where
+    fmap f (FailCont cont) = FailCont $ \k ->
+        cont (\result -> k (fmap f result))
 
 instance Applicative (FailCont r e) where
+    pure x = FailCont $ \k -> k (Right x)
+    FailCont f <*> FailCont x = FailCont $ \k ->
+        f (\fResult -> x (\xResult -> k (fResult <*> xResult)))
 
 instance Monad (FailCont r e) where
+    FailCont m >>= f = FailCont $ \k ->
+        m (\result -> case result of
+            Left err -> k (Left err)
+            Right val -> runFailCont (f val) k)
--- monads/src/FailCont/Main.hs
+++ monads/src/FailCont/Main.hs
@@ -1,21 +1,42 @@
 module FailCont.Main where
 
-import FailCont.FailCont 
+import FailCont.FailCont
 import Text.Read (readMaybe)
 
-data Error 
-  = EmptyInput 
-  | ParseFailed String 
+data Error
+  = EmptyInput
+  | ParseFailed String
   | DivisionByZero
+  | NegativeInput
   deriving (Show, Eq)
 
 addInts :: String -> String -> FailCont r Error Int
-addInts = undefined 
+addInts x y =
+    case (readMaybe x :: Maybe Int, readMaybe y :: Maybe Int) of
+        (Nothing, _) -> toFailCont (Left EmptyInput)
+        (_, Nothing) -> toFailCont (Left (ParseFailed y))
+        (Just a, Just b) -> toFailCont (Right (a + b))
 
-divInts :: String -> String -> FailCont r Error Int 
-divInts = undefined 
+divInts :: String -> String -> FailCont r Error Int
+divInts x y =
+    case (readMaybe x :: Maybe Int, readMaybe y :: Maybe Int) of
+        (Nothing, _) -> toFailCont (Left EmptyInput)
+        (_, Nothing) -> toFailCont (Left (ParseFailed y))
+        (Just a, Just 0) -> toFailCont (Left DivisionByZero)
+        (Just a, Just b) -> toFailCont (Right (a `div` b))
 
-main = do 
+factorial :: String -> FailCont r Error Int
+factorial input =
+    case readMaybe input :: Maybe Int of
+        Nothing -> toFailCont (Left $ ParseFailed input)
+        Just n
+            | n < 0     -> toFailCont (Left (NegativeInput))
+            | otherwise -> toFailCont (Right (fact n))
+    where
+    fact 0 = 1
+    fact n = n * fact (n - 1)
+
+main = do
   print $ evalFailCont $ addInts "13" "42"         -- Right 55
   print $ evalFailCont $ addInts "" "42"           -- Left EmptyInput
   print $ evalFailCont $ addInts "13" "fourty two" -- Left (ParseFailed "fourty two")
@@ -24,3 +45,7 @@ main = do
   print $ evalFailCont $ divInts "13" "0"          -- Left DivisionByZero
   print $ evalFailCont $ divInts "13" "000"        -- Left DivisionByZero
   print $ evalFailCont $ divInts "" "42"           -- Left EmptyInput
+  print $ evalFailCont $ factorial "10"            -- Right 3628800
+  print $ evalFailCont $ factorial "aaaaa"         -- Left (ParseFailed "aaaaa")
+  print $ evalFailCont $ factorial "-3"           -- Left NegativeInput
+  print $ evalFailCont $ factorial ""             -- Left (ParseFailed "")
