5981b3502d0febd9067425a407ae623f2e6eb4f4
Nikita Panuhin <github@npanuhin.me>
2024-11-07 22:07:40 +0100

    Homework Solution

--- monads/src/FailCont/FailCont.hs
+++ monads/src/FailCont/FailCont.hs
@@ -3,16 +3,25 @@ module FailCont.FailCont where
 -- r is the result 
 -- e is the type of a failure
 -- a is the type of a success
-newtype FailCont r e a = undefined 
+newtype FailCont r e a = FailCont { runFailCont :: (a -> r) -> (e -> r) -> r }
 
 toFailCont :: Either e a -> FailCont r e a
-toFailCont = undefinedd 
+toFailCont (Left e) = FailCont $ \_ failure -> failure e
+toFailCont (Right a) = FailCont $ \success _ -> success a
 
 evalFailCont :: FailCont (Either e a) e a -> Either e a
-evalFailCont = undefined 
+evalFailCont (FailCont run) = run Right Left
 
 instance Functor (FailCont r e) where
+  fmap f (FailCont run) = FailCont $ \success failure ->
+    run (success . f) failure
 
 instance Applicative (FailCont r e) where
+  pure x = FailCont $ \success _ -> success x
+
+  (FailCont f) <*> (FailCont x) = FailCont $ \success failure ->
+    f (\g -> x (success . g) failure) failure
 
 instance Monad (FailCont r e) where
+  (FailCont run) >>= f = FailCont $ \success failure ->
+    run (\a -> runFailCont (f a) success failure) failure
--- monads/src/FailCont/Main.hs
+++ monads/src/FailCont/Main.hs
@@ -9,11 +9,31 @@ data Error
   | DivisionByZero
   deriving (Show, Eq)
 
-addInts :: String -> String -> FailCont r Error Int
-addInts = undefined 
+parseInput :: String -> FailCont r Error Int
+parseInput "" = toFailCont $ Left EmptyInput
+parseInput s  = toFailCont $ maybe (Left (ParseFailed s)) Right (readMaybe s)
 
-divInts :: String -> String -> FailCont r Error Int 
-divInts = undefined 
+addInts :: String -> String -> FailCont r Error Int
+addInts x y = do
+  a <- parseInput x
+  b <- parseInput y
+  return (a + b)
+
+divInts :: String -> String -> FailCont r Error Int
+divInts x y = do
+  a <- parseInput x
+  b <- parseInput y
+  toFailCont $ if b == 0
+               then Left DivisionByZero
+               else Right (a `div` b)
+
+sumRange :: String -> String -> FailCont r Error Int
+sumRange x y = do
+  a <- parseInput x
+  b <- parseInput y
+  toFailCont $ if a > b
+               then Left (ParseFailed "a must be less than or equal to b")
+               else Right (sum [a..b])
 
 main = do 
   print $ evalFailCont $ addInts "13" "42"         -- Right 55
@@ -24,3 +44,9 @@ main = do
   print $ evalFailCont $ divInts "13" "0"          -- Left DivisionByZero
   print $ evalFailCont $ divInts "13" "000"        -- Left DivisionByZero
   print $ evalFailCont $ divInts "" "42"           -- Left EmptyInput
+  print $ evalFailCont $ sumRange "1" "5"          -- Right 15 {1 + 2 + 3 + 4 + 5}
+  print $ evalFailCont $ sumRange "5" "1"          -- Left (ParseFailed "a must be less than or equal to b")
+  print $ evalFailCont $ sumRange "10" "10"        -- Right 10
+  print $ evalFailCont $ sumRange "" "5"           -- Left EmptyInput
+  print $ evalFailCont $ sumRange "a" "5"          -- Left (ParseFailed "a")
+  print $ evalFailCont $ sumRange "1" "b"          -- Left (ParseFailed "b")
