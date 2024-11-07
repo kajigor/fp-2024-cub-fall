a55d5dadfdd8b285e94e5a49620a7eefa2deb930
joseserize0222 <joseserize0222@gmail.com>
2024-11-07 23:44:09 +0100

    Serize HW06

--- monads/src/FailCont/FailCont.hs
+++ monads/src/FailCont/FailCont.hs
@@ -1,18 +1,56 @@
+{-# LANGUAGE InstanceSigs #-}
 module FailCont.FailCont where 
 
 -- r is the result 
 -- e is the type of a failure
 -- a is the type of a success
-newtype FailCont r e a = undefined 
+newtype FailCont r e a = FailCont { runFailCont :: (e -> r) -> (a -> r) -> r }
 
 toFailCont :: Either e a -> FailCont r e a
-toFailCont = undefinedd 
+toFailCont (Left e) = FailCont $ \fail _ -> fail e
+toFailCont (Right a) = FailCont $ \_ success -> success a
+
 
 evalFailCont :: FailCont (Either e a) e a -> Either e a
-evalFailCont = undefined 
+evalFailCont (FailCont x) = x Left Right
+-- x :: (e -> (Either e a)) -> (a -> (Either e a)) -> (Either e a)
+
+
 
 instance Functor (FailCont r e) where
+    fmap :: (a -> b) -> FailCont r e a -> FailCont r e b
+    -- x ::    (e -> r) -> (a -> r) -> r
+    -- fail :: (e -> r)
+    -- f ::                (a -> b)
+    -- success ::               (b -> r)
+    
+    fmap f (FailCont x) = FailCont $ \fail success  -> x fail (success . f)
+    
 
 instance Applicative (FailCont r e) where
+    pure :: a -> FailCont r e a
+    -- success :: (a -> r)
+    pure x = FailCont $ \_ success  -> success x
+    (<*>) :: FailCont r e (a -> b) -> FailCont r e a -> FailCont r e b
+    -- ff ::   (e -> r) -> ((a -> b -> r)) -> r
+    -- x ::    (e -> r) -> (a -> r) -> r
+    -- fail :: (e -> r)
+    -- f ::                (a -> b -> r)
+    -- success ::               (b -> r)
+
+    (FailCont ff) <*> (FailCont x) = FailCont $ \fail success  ->
+        ff fail (\f -> x fail (success . f)) 
 
 instance Monad (FailCont r e) where
+    (>>=) :: FailCont r e a -> (a -> FailCont r e b) -> FailCont r e b
+    -- f :: a -> FailCont r e b
+    -- x ::    (e -> r) -> (a -> r) -> r
+    -- runFailCont         (f a) :: (e -> r) -> (b -> r) -> r
+    -- fail ::                      (e -> r)
+    -- success ::                               (b -> r)
+
+    (FailCont x) >>= f = FailCont $ \fail success  ->
+        x fail (\a -> runFailCont (f a) fail success ) 
+
+
+
--- monads/src/FailCont/Main.hs
+++ monads/src/FailCont/Main.hs
@@ -9,11 +9,45 @@ data Error
   | DivisionByZero
   deriving (Show, Eq)
 
-addInts :: String -> String -> FailCont r Error Int
-addInts = undefined 
+parse :: String -> FailCont r Error Int
+parse str = 
+    case str of
+      "" -> toFailCont $ Left EmptyInput
+      _  -> case readMaybe str of
+              Nothing -> toFailCont $ Left $ ParseFailed str 
+              Just value -> toFailCont $ Right value
 
-divInts :: String -> String -> FailCont r Error Int 
-divInts = undefined 
+addInts :: String -> String -> FailCont r Error Int
+addInts x y = do
+    a <- parse x
+    b <- parse y
+    return $ a + b
+
+divInts :: String -> String -> FailCont r Error Int
+divInts x y = do
+    a <- parse x
+    b <- parse y 
+    if b == 0 then toFailCont $ Left DivisionByZero else toFailCont $ Right $ a `div` b
+
+-- Example of usage
+
+data FormError = EmptyName | InvalidAge String deriving (Show, Eq)
+
+validateName :: String -> FailCont r FormError String
+validateName "" = toFailCont $ Left EmptyName
+validateName name = toFailCont $ Right name
+
+validateAge :: String -> FailCont r FormError Int
+validateAge ageStr =
+    case readMaybe ageStr of
+        Nothing -> toFailCont $ Left (InvalidAge ageStr)
+        Just age -> toFailCont $ Right age
+
+validateForm :: String -> String -> FailCont r FormError (String, Int)
+validateForm name ageStr = do
+    validName <- validateName name
+    validAge <- validateAge ageStr
+    return (validName, validAge)
 
 main = do 
   print $ evalFailCont $ addInts "13" "42"         -- Right 55
@@ -24,3 +58,6 @@ main = do
   print $ evalFailCont $ divInts "13" "0"          -- Left DivisionByZero
   print $ evalFailCont $ divInts "13" "000"        -- Left DivisionByZero
   print $ evalFailCont $ divInts "" "42"           -- Left EmptyInput
+  print $ evalFailCont $ validateForm "Alice" "25"         -- Right ("Alice", 25)
+  print $ evalFailCont $ validateForm "" "25"              -- Left EmptyName
+  print $ evalFailCont $ validateForm "Alice" "notAnAge"   -- Left (InvalidAge "notAnAge")
