8414ff46ec61bd18fee2ba38bd63489b81d74134
Stanislav.Sandler <stanislav.sandler@jetbrains.com>
2024-11-08 00:19:41 +0100

    Homework done

--- monads/src/FailCont/FailCont.hs
+++ monads/src/FailCont/FailCont.hs
@@ -3,16 +3,38 @@ module FailCont.FailCont where
 -- r is the result 
 -- e is the type of a failure
 -- a is the type of a success
-newtype FailCont r e a = undefined 
+newtype FailCont r e a = FailCont { runFailCont :: (Either e a -> r) -> r }
 
 toFailCont :: Either e a -> FailCont r e a
-toFailCont = undefinedd 
+toFailCont v = FailCont $ \f -> f v 
 
 evalFailCont :: FailCont (Either e a) e a -> Either e a
-evalFailCont = undefined 
+evalFailCont fc = runFailCont fc id 
 
 instance Functor (FailCont r e) where
+    fmap :: (a -> b) -> FailCont r e a -> FailCont r e b
+    fmap f (FailCont fc) = FailCont $ \u -> fc (\v -> u (fmap f v))
 
 instance Applicative (FailCont r e) where
+    pure :: a -> FailCont r e a
+    pure x = FailCont $ \f -> f (Right x)
+    
+    (<*>) :: FailCont r e (a -> b) -> FailCont r e a -> FailCont r e b
+    FailCont ff <*> FailCont fu = FailCont $ \v ->
+        ff $ \f -> 
+        fu $ \u -> 
+        v (case f of
+            Left e  -> Left e
+            Right fn -> fmap fn u)
+
 
 instance Monad (FailCont r e) where
+    return :: a -> FailCont r e a
+    return = pure
+
+    (>>=) :: FailCont r e a -> (a -> FailCont r e b) -> FailCont r e b
+    FailCont fu >>= g = FailCont $ \v ->
+        fu $ \u ->
+            case u of 
+                Left e -> v $ Left e
+                Right val -> runFailCont (g val) v 
--- monads/src/FailCont/Main.hs
+++ monads/src/FailCont/Main.hs
@@ -7,13 +7,37 @@ data Error
   = EmptyInput 
   | ParseFailed String 
   | DivisionByZero
+  | NegativeRadius
   deriving (Show, Eq)
 
+liftMaybe :: Error -> Maybe a -> FailCont r Error a
+liftMaybe err Nothing  = FailCont $ \c -> c (Left err)
+liftMaybe _ (Just x) = pure x
+
+calculateSurface :: String -> FailCont r Error Double
+calculateSurface s = do
+  rd <- liftMaybe (ParseFailed s) (readMaybe s)
+  if rd < 0 
+    then FailCont $ \f -> f $ Left NegativeRadius
+    else return $ pi * rd * rd
+
 addInts :: String -> String -> FailCont r Error Int
-addInts = undefined 
+addInts "" s2 = FailCont $ \f -> f (Left EmptyInput)
+addInts s1 "" = FailCont $ \f -> f (Left EmptyInput)
+addInts s1 s2 = do
+  a <- liftMaybe (ParseFailed s1) (readMaybe s1)
+  b <- liftMaybe (ParseFailed s2) (readMaybe s2)
+  return (a + b)
 
 divInts :: String -> String -> FailCont r Error Int 
-divInts = undefined 
+divInts "" s2 = FailCont $ \f -> f (Left EmptyInput)
+divInts s1 "" = FailCont $ \f -> f (Left EmptyInput)
+divInts s1 s2 = do
+  a <- liftMaybe (ParseFailed s1) (readMaybe s1)
+  b <- liftMaybe (ParseFailed s2) (readMaybe s2)
+  if b == 0 
+    then FailCont $ \f -> f (Left DivisionByZero)
+    else return (a `div` b)
 
 main = do 
   print $ evalFailCont $ addInts "13" "42"         -- Right 55
@@ -24,3 +48,5 @@ main = do
   print $ evalFailCont $ divInts "13" "0"          -- Left DivisionByZero
   print $ evalFailCont $ divInts "13" "000"        -- Left DivisionByZero
   print $ evalFailCont $ divInts "" "42"           -- Left EmptyInput
+  print $ evalFailCont $ calculateSurface "-1"     -- Left NegativeRadius
+  print $ evalFailCont $ calculateSurface "1"      -- Right 3.141592653589793
