cf3478a6776a01e09d6a312e8921927597d1f184
Vladimir Lezhnev <vladimir.lezhnev@jetbrains.com>
2024-12-05 23:53:15 +0100

    code cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -6,23 +6,20 @@ import Control.Monad.IO.Class (liftIO)
 import qualified Sudoku as S
 import qualified Data.Text as T
 import qualified Data.Text.Lazy as TL
-import Network.HTTP.Types (status400)  -- Import HTTP status codes
+import Network.HTTP.Types (status400)
 
 main :: IO ()
 main = scotty 3000 $ do
-    -- Enable CORS for frontend interaction
     middleware simpleCors
 
-    -- Route for generating a new Sudoku puzzle
     get "/generate" $ do
         difficultyStr <- param "difficulty" `rescue` (\_ -> return "easy")
-        let difficulty = T.pack difficultyStr  -- Convert String to Text
+        let difficulty = T.pack difficultyStr
         liftIO $ putStrLn $ "Generating Sudoku with difficulty: " ++ difficultyStr
         puzzle <- liftIO $ S.generateSudoku difficulty
         liftIO $ putStrLn $ "Generated Sudoku: " ++ show puzzle
         text $ S.encodeGrid puzzle
 
-    -- Route for checking the validity of a Sudoku grid
     post "/check" $ do
         formData <- params
         let strictFormData = map (\(k, v) -> (S.toStrict k, S.toStrict v)) formData
@@ -35,7 +32,6 @@ main = scotty 3000 $ do
                           else do
                               text "Invalid"
 
-    -- Route for solving a Sudoku puzzle
     post "/solve" $ do
         formData <- params
         let strictFormData = map (\(k, v) -> (S.toStrict k, S.toStrict v)) formData
