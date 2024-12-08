0b03ea53c11f239449403e0e62dfce48db51552a
abanoubseddik <abanoub.maged171@gmail.com>
2024-12-08 22:37:52 +0100

    Fixed win conditions

--- Minesweeper/src/ConsoleUI.hs
+++ Minesweeper/src/ConsoleUI.hs
@@ -26,7 +26,9 @@ initializeAndStartGame rows cols mines = do
 -- Game loop to handle moves
 gameLoop :: Grid -> IO ()
 gameLoop grid = do
+    let minesLeft = calculateMinesLeft grid
     printGrid grid
+    putStrLn $ "Mines left: " ++ show minesLeft
     putStr "Enter move (row col action [r/f]): "
     hFlush stdout
     input <- getLine
@@ -34,12 +36,38 @@ gameLoop grid = do
         Just (r, c, 'r') ->
             let newGrid = revealCell grid (r, c)
             in if isLoss newGrid
-               then putStrLn "Game Over!" >> printGrid newGrid
+               then do
+                   putStrLn $ bigMessage "Game Over!"
+                   printGrid newGrid
                else if isWin newGrid
-                    then putStrLn "You win!" >> printGrid newGrid
+                    then do
+                        putStrLn $ bigMessage "You win!"
+                        printGrid newGrid
                     else gameLoop newGrid
         Just (r, c, 'f') -> gameLoop (flagCell grid (r, c))
         _ -> putStrLn "Invalid input." >> gameLoop grid
+
+
+bigMessage :: String -> String
+bigMessage "Game Over!" = unlines
+    [ "    ______   _________    __  _________   ____ _    ____________     ______",
+      "   / / / /  / ____/   |  /  |/  / ____/  / __ \\ |  / / ____/ __ \\   / / / /",
+      "  / / / /  / / __/ /| | / /|_/ / __/    / / / / | / / __/ / /_/ /  / / / / ",
+      " /_/_/_/  / /_/ / ___ |/ /  / / /___   / /_/ /| |/ / /___/ _, _/  /_/_/_/  ",
+      "(_|_|_)   \\____/_/  |_/_/  /_/_____/   \\____/ |___/_____/_/ |_|  (_|_|_)   ",
+      "                                                                            "
+    ]
+bigMessage "You win!" = unlines
+    [ "     __  __  __    __  ______  __  __   _       _______   __    __  __  __",
+      "   _/ /_/ /_/ /    \\ \\/ / __ \\/ / / /  | |     / /  _/ | / /  _/ /_/ /_/ /",
+      "  / __/ __/ __/     \\  / / / / / / /   | | /| / // //  |/ /  / __/ __/ __/",
+      " (_  |_  |_  )      / / /_/ / /_/ /    | |/ |/ // // /|  /  (_  |_  |_  ) ",
+      " /  _/  _/  _/      /_/\\____/\\____/     |__/|__/___/_/ |_/  /  _/  _/  _/  ",
+      " /_/ /_/ /_/                                                /_/ /_/ /_/     "
+    ]
+bigMessage _ = "Invalid Message"
+
+
 
 -- Parse a move from input
 parseMove :: String -> Maybe (Int, Int, Char)
